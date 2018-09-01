976ddb61fecbac704e4cfc07c66e217a4af4b50b
dielduarte <magdiel.kesller@gmail.com>
2018-09-02 01:49:42 +0200

    initial commit :rocket:

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>hey</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  }
+});
